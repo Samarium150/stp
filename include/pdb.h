4ba94e6b93de55c20ec51f60e94ea352560249ec
Samarium150 <28302241+Samarium150@users.noreply.github.com>
2025-03-08 16:21:25 -0700

    refactor: extract the ranking function

--- include/pdb.h
+++ include/pdb.h
@@ -7,6 +7,7 @@
 #include "blockingconcurrentqueue.h"
 #include "packed_storage.h"
 #include "puzzle.h"
+#include "ranking.h"
 
 namespace stp::algorithm {
 
@@ -16,10 +17,7 @@ public:
     template <typename Container,
               std::enable_if_t<std::is_convertible_v<typename Container::value_type, uint8_t>,
                                bool> = true>
-    PatternDatabase(Puzzle<width, height>& puzzle, const Container& pattern)
-        : puzzle_(puzzle),
-          num_threads_(std::thread::hardware_concurrency()),
-          thread_data_(num_threads_) {
+    PatternDatabase(Puzzle<width, height>& puzzle, const Container& pattern) : puzzle_(puzzle) {
         const auto pattern_size = static_cast<uint16_t>(pattern.size());
         if (pattern_size == 0 || pattern_size > width * height) {
             throw std::invalid_argument("the pattern size must be between 1 and the puzzle size");
@@ -34,34 +32,22 @@ public:
     }
 
     PatternDatabase(const PatternDatabase& pdb)
-        : puzzle_(pdb.GetPuzzle()),
-          pattern_(pdb.Pattern()),
-          db_(pdb.DB()),
-          num_threads_(std::thread::hardware_concurrency()),
-          thread_data_(num_threads_) {}
+        : puzzle_(pdb.GetPuzzle()), pattern_(pdb.Pattern()), db_(pdb.DB()) {}
 
     PatternDatabase& operator=(const PatternDatabase& pdb) {
         puzzle_ = pdb.GetPuzzle();
         pattern_ = pdb.Pattern();
         db_ = pdb.DB();
-        num_threads_ = std::thread::hardware_concurrency();
-        thread_data_ = std::vector<ThreadData>(num_threads_);
         return *this;
     }
 
     PatternDatabase(PatternDatabase&& pdb) noexcept
-        : puzzle_(pdb.GetPuzzle()),
-          pattern_(std::move(pdb.Pattern())),
-          db_(std::move(pdb.DB())),
-          num_threads_(std::thread::hardware_concurrency()),
-          thread_data_(num_threads_) {}
+        : puzzle_(pdb.GetPuzzle()), pattern_(std::move(pdb.Pattern())), db_(std::move(pdb.DB())) {}
 
     PatternDatabase& operator=(PatternDatabase&& pdb) noexcept {
         puzzle_ = pdb.GetPuzzle();
         pattern_ = std::move(pdb.Pattern());
         db_ = std::move(pdb.DB());
-        num_threads_ = std::thread::hardware_concurrency();
-        thread_data_ = std::vector<ThreadData>(num_threads_);
         return *this;
     }
 
@@ -134,6 +120,7 @@ public:
         constexpr auto MAX_COST = std::numeric_limits<uint8_t>::max();
         const auto size = db_.Size();
         constexpr std::size_t CHUNK_SIZE = 4096;
+        const auto num_thread = std::thread::hardware_concurrency();
 
         std::array<std::vector<std::atomic_bool>, MAX_COST + 1> buckets;
         std::ranges::for_each(buckets, [size](auto& bucket) {
@@ -142,12 +129,13 @@ public:
 
         Queue tasks((size + CHUNK_SIZE - 1) / CHUNK_SIZE);
 
-        const auto goal = GetPDBHash(puzzle.Goal());
+        const auto goal = util::Ranking(puzzle.Goal(), pattern_);
         db_.Set(goal, 0u);
         buckets[0][goal / CHUNK_SIZE] = true;
-        std::vector<std::jthread> threads(num_threads_);
+
+        std::vector<std::jthread> threads(num_thread);
         std::atomic_size_t visited = 1;
-        std::barrier barrier(num_threads_ + 1);
+        std::barrier barrier(num_thread + 1);
         for (uint8_t current_cost = 0; current_cost <= MAX_COST && visited < size; ++current_cost) {
             auto& bucket = buckets[current_cost];
             for (std::size_t i = 0; i < size; i += CHUNK_SIZE) {
@@ -156,103 +144,70 @@ public:
                 }
             }
             bucket.clear();
-            for (std::size_t thread_id = 0; thread_id < num_threads_; ++thread_id) {
-                threads.emplace_back(
-                    [this, &buckets, &tasks, &visited, current_cost, thread_id, &barrier] {
-                        uint64_t count = 0;
-                        std::vector<std::pair<uint64_t, uint8_t>> successors;
-                        std::vector<std::pair<uint64_t, uint8_t>> zero_cost_successors;
-                        std::pair<uint64_t, uint64_t> range{0, 0};
-                        while (tasks.try_dequeue(range)) {
-                            const auto [start, end] = range;
-                            for (uint64_t hash = start; hash < end; ++hash) {
-                                if (const auto cost = db_.GetAtomic(hash); cost == current_cost) {
-                                    GetSuccessors(hash, cost, successors, thread_id);
-                                }
-                            }
-                            while (!successors.empty()) {
-                                for (const auto& [hash, cost] : successors) {
-                                    uint8_t record = db_.GetAtomic(hash);
-                                    while (cost < record) {
-                                        if (db_.CompareAndSet(hash, record, cost)) {
-                                            if (record == std::numeric_limits<uint8_t>::max()) {
-                                                ++count;
-                                            }
-                                            if (cost == current_cost) {
-                                                zero_cost_successors.emplace_back(hash, cost);
-                                            } else {
-                                                buckets[cost][hash / CHUNK_SIZE].store(
-                                                    true, std::memory_order_relaxed);
-                                            }
-                                            break;
-                                        }
-                                        record = db_.GetAtomic(hash);
-                                        if (cost >= record) {
-                                            break;
-                                        }
-                                    }
-                                }
-                                successors.clear();
-                                for (const auto& [hash, cost] : zero_cost_successors) {
-                                    GetSuccessors(hash, cost, successors, thread_id);
-                                }
-                                zero_cost_successors.clear();
+            for (std::size_t i = 0; i < num_thread; ++i) {
+                threads.emplace_back([this, &buckets, &tasks, &visited, current_cost, &barrier] {
+                    uint64_t count = 0;
+                    std::vector<std::pair<uint64_t, uint8_t>> successors;
+                    std::vector<std::pair<uint64_t, uint8_t>> zero_cost_successors;
+                    std::pair<uint64_t, uint64_t> range{0, 0};
+                    while (tasks.try_dequeue(range)) {
+                        const auto [start, end] = range;
+                        for (uint64_t hash = start; hash < end; ++hash) {
+                            if (const auto cost = db_.GetAtomic(hash); cost == current_cost) {
+                                GetSuccessors(hash, cost, successors);
                             }
                         }
-                        visited.fetch_add(count, std::memory_order_relaxed);
-                        barrier.arrive_and_wait();
-                    });
+                        while (!successors.empty()) {
+                            for (const auto& [hash, cost] : successors) {
+                                uint8_t record = db_.GetAtomic(hash);
+                                while (cost < record) {
+                                    if (db_.CompareAndSet(hash, record, cost)) {
+                                        if (record == std::numeric_limits<uint8_t>::max()) {
+                                            ++count;
+                                        }
+                                        if (cost == current_cost) {
+                                            zero_cost_successors.emplace_back(hash, cost);
+                                        } else {
+                                            buckets[cost][hash / CHUNK_SIZE].store(
+                                                true, std::memory_order_relaxed);
+                                        }
+                                        break;
+                                    }
+                                    record = db_.GetAtomic(hash);
+                                    if (cost >= record) {
+                                        break;
+                                    }
+                                }
+                            }
+                            successors.clear();
+                            for (const auto& [hash, cost] : zero_cost_successors) {
+                                GetSuccessors(hash, cost, successors);
+                            }
+                            zero_cost_successors.clear();
+                        }
+                    }
+                    visited.fetch_add(count, std::memory_order_relaxed);
+                    barrier.arrive_and_wait();
+                });
             }
             barrier.arrive_and_wait();
             threads.clear();
         }
     }
 
-    uint64_t GetPDBHash(const State<width, height>& state,
-                        const std::optional<std::size_t> thread_id = std::nullopt) const noexcept {
-        if (thread_id.has_value()) {
-            auto& thread_data = thread_data_[thread_id.value()];
-            auto& dual = thread_data.dual;
-            auto& locations = thread_data.locations;
-            auto& values = thread_data.values;
-            std::ranges::fill(dual, 0);
-            std::ranges::fill(locations, 0);
-            return Ranking(state, dual, locations, values);
-        }
-        thread_local std::array<uint8_t, width * height> dual{};
-        thread_local std::array<uint8_t, width * height> locations{};
-        thread_local std::deque<uint8_t> values{};
-        return Ranking(state, dual, locations, values);
-    }
-
-    void GetStateFromHash(
-        const uint64_t hash, State<width, height>& state,
-        const std::optional<std::size_t> thread_id = std::nullopt) const noexcept {
-        if (thread_id.has_value()) {
-            auto& thread_data = thread_data_[thread_id.value()];
-            auto& dual = thread_data.dual;
-            UnRanking(hash, state, dual);
-            return;
-        }
-        thread_local std::array<uint8_t, width * height> dual{};
-        UnRanking(hash, state, dual);
-    }
-
     void GetSuccessors(const uint64_t hash, const uint8_t cost,
-                       std::vector<std::pair<uint64_t, uint8_t>>& successors,
-                       const std::size_t thread_id = 0) const noexcept {
+                       std::vector<std::pair<uint64_t, uint8_t>>& successors) const noexcept {
         auto state = State(true);
-        GetStateFromHash(hash, state, thread_id);
-        auto& actions = thread_data_[thread_id].actions;
+        util::UnRanking(hash, state, pattern_);
+        std::vector<Action> actions;
         puzzle_.get().GetActions(state, actions);
         for (auto& action : actions) {
             const auto action_cost = puzzle_.get().ApplyAction(state, action);
             const auto successor_cost = static_cast<uint8_t>(cost + action_cost);
-            const auto successor_hash = GetPDBHash(state, thread_id);
+            const auto successor_hash = util::Ranking(state, pattern_);
             successors.emplace_back(successor_hash, successor_cost);
             puzzle_.get().UndoAction(state, action);
         }
-        actions.clear();
     }
 
     static uint64_t FactorialUpperK(const unsigned n, const unsigned k) {
@@ -263,98 +218,18 @@ public:
         return value;
     }
 
-    unsigned HCost(const State<width, height>& state) const { return db_.Get(GetPDBHash(state)); }
+    unsigned HCost(const State<width, height>& state) const {
+        return db_.Get(util::Ranking(state, pattern_));
+    }
 
 private:
-    /**
-     * One of the linear time ranking functions
-     * designed by Wendy Myrvold and Frank Ruskey in their paper
-     * [Ranking and unranking permutations in linear time]
-     * @tparam C1 Container type for the dual
-     * @tparam C2 Container type for the locations
-     * @tparam C3 Container type for the values
-     * @param state the state to rank
-     * @param dual the cache for the `dual` representation of the abstract state
-     * @param locations the cache for store indices of pattern tiles
-     * @param values the cache for the `swapped` index
-     * @return the rank of the state
-     * @see https://doi.org/10.1016/S0020-0190(01)00141-7
-     */
-    template <typename C1, typename C2, typename C3>
-    uint64_t Ranking(const State<width, height>& state, C1& dual, C2& locations,
-                     C3& values) const noexcept {
-        const auto puzzle_size = static_cast<uint16_t>(state.Size());
-        for (auto i = 0; i < puzzle_size; ++i) {
-            if (state[i] != -1) {
-                dual[static_cast<std::size_t>(state[i])] = static_cast<uint8_t>(i);
-            }
-        }
-        const auto pattern_size = static_cast<uint8_t>(pattern_.size());
-        for (uint8_t i = 0; i < pattern_size; ++i) {
-            locations[puzzle_size - i - 1] = dual[pattern_[pattern_size - i - 1]];
-        }
-        std::ranges::fill(dual, 0);
-        const uint16_t last = puzzle_size - pattern_size;
-        for (auto i = last; i < puzzle_size; ++i) {
-            dual[locations[i]] = static_cast<uint8_t>(i);
-        }
-        for (uint8_t i = puzzle_size - 1; i >= last; --i) {
-            values.emplace_back(locations[i]);
-            std::swap(locations[i], locations[dual[i]]);
-            std::swap(dual[values.back()], dual[i]);
-        }
-        uint64_t hash = 0;
-        uint8_t count = last + 1;
-        while (!values.empty()) {
-            hash *= count++;
-            hash += values.back();
-            values.pop_back();
-        }
-        return hash;
-    }
-
-    /**
-     * One of the linear time unranking functions
-     * designed by Wendy Myrvold and Frank Ruskey in their paper
-     * [Ranking and unranking permutations in linear time]
-     * @tparam Container type for the dual
-     * @param hash the hash for unranking
-     * @param state the output state
-     * @param dual the cache for the `dual` representation of the abstract state
-     * @see https://doi.org/10.1016/S0020-0190(01)00141-7
-     */
-    template <typename Container>
-    void UnRanking(uint64_t hash, State<width, height>& state, Container& dual) const noexcept {
-        const auto puzzle_size = static_cast<uint16_t>(state.Size());
-        for (auto i = 0; i < puzzle_size; ++i) {
-            dual[i] = static_cast<uint8_t>(i);
-        }
-        const auto last = static_cast<uint8_t>(puzzle_size - pattern_.size());
-        for (auto i = static_cast<uint8_t>(puzzle_size - 1); i >= last; --i) {
-            std::swap(dual[hash % (i + 1)], dual[i]);
-            hash /= (i + 1);
-            state[dual[i]] = pattern_[i - last];
-        }
-        state.UpdateBlankPosition();
-    }
-
     using State = State<width, height>;
     using Puzzle = Puzzle<width, height>;
-    std::reference_wrapper<Puzzle> puzzle_;
-
-    std::vector<uint8_t> pattern_{};
-    util::PackedStorage<> db_{};
-
     using Queue = moodycamel::ConcurrentQueue<std::pair<uint64_t, uint64_t>>;
 
-    std::size_t num_threads_;
-    struct alignas(64) ThreadData {
-        std::array<uint8_t, width * height> dual{};
-        std::array<uint8_t, width * height> locations{};
-        std::deque<uint8_t> values{};
-        std::vector<Action> actions{};
-    };
-    mutable std::vector<ThreadData> thread_data_;
+    std::reference_wrapper<Puzzle> puzzle_;
+    std::vector<uint8_t> pattern_{};
+    util::PackedStorage<> db_{};
 };
 
 template <uint8_t width, uint8_t height>
---
+++ include/ranking.h
@@ -0,0 +1,89 @@
+#ifndef RANKING_H_
+#define RANKING_H_
+
+#include "puzzle.h"
+
+namespace stp::util {
+
+constexpr auto default_pattern = []<uint8_t width, uint8_t height> {
+    const auto& data = State<width, height>().Data();
+    return std::vector<uint8_t>{data.begin(), data.end()};
+};
+
+/**
+ * One of the linear time ranking functions
+ * designed by Wendy Myrvold and Frank Ruskey in their paper
+ * [Ranking and unranking permutations in linear time]
+ * @param state the state to rank
+ * @param pattern_opt the optional pattern of tiles
+ * @return the rank of the state
+ * @see https://doi.org/10.1016/S0020-0190(01)00141-7
+ */
+template <uint8_t width, uint8_t height>
+    requires(width > 0 && width <= 16 && height > 0 && height <= 16)
+uint64_t Ranking(const State<width, height>& state,
+                 const std::optional<std::vector<uint8_t>>& pattern_opt = std::nullopt) noexcept {
+    thread_local std::array<uint8_t, width * height> dual;
+    thread_local std::array<uint8_t, width * height> locations;
+    thread_local std::deque<uint8_t> values;
+    const auto pattern = pattern_opt.value_or(default_pattern.operator()<width, height>());
+    const auto puzzle_size = static_cast<uint16_t>(state.Size());
+    for (auto i = 0; i < puzzle_size; ++i) {
+        if (state[i] != -1) {
+            dual[static_cast<std::size_t>(state[i])] = static_cast<uint8_t>(i);
+        }
+    }
+    const auto pattern_size = static_cast<uint16_t>(pattern.size());
+    for (auto i = 0; i < pattern_size; ++i) {
+        locations[puzzle_size - i - 1] = dual[pattern[pattern_size - i - 1]];
+    }
+    std::ranges::fill(dual, 0);
+    const uint16_t last = puzzle_size - pattern_size;
+    for (auto i = last; i < puzzle_size; ++i) {
+        dual[locations[i]] = static_cast<uint8_t>(i);
+    }
+    for (auto i = puzzle_size - 1; i >= last; --i) {
+        values.emplace_back(locations[i]);
+        std::swap(locations[i], locations[dual[i]]);
+        std::swap(dual[values.back()], dual[i]);
+    }
+    uint64_t hash = 0;
+    uint8_t count = last + 1;
+    while (!values.empty()) {
+        hash *= count++;
+        hash += values.back();
+        values.pop_back();
+    }
+    return hash;
+}
+
+/**
+ * One of the linear time unranking functions
+ * designed by Wendy Myrvold and Frank Ruskey in their paper
+ * [Ranking and unranking permutations in linear time]
+ * @param hash the hash for unranking
+ * @param state the output state
+ * @param pattern_opt the optional pattern of tiles
+ * @see https://doi.org/10.1016/S0020-0190(01)00141-7
+ */
+template <uint8_t width, uint8_t height>
+    requires(width > 0 && width <= 16 && height > 0 && height <= 16)
+void UnRanking(uint64_t hash, State<width, height>& state,
+               const std::optional<std::vector<uint8_t>>& pattern_opt = std::nullopt) noexcept {
+    thread_local std::array<uint8_t, width * height> dual;
+    const auto pattern = pattern_opt.value_or(default_pattern.operator()<width, height>());
+    const auto puzzle_size = static_cast<uint16_t>(state.Size());
+    for (auto i = 0; i < puzzle_size; ++i) {
+        dual[i] = static_cast<uint8_t>(i);
+    }
+    const auto last = static_cast<uint16_t>(puzzle_size - pattern.size());
+    for (auto i = puzzle_size - 1; i >= last; --i) {
+        std::swap(dual[hash % (i + 1)], dual[i]);
+        hash /= (i + 1);
+        state[dual[i]] = pattern[i - last];
+    }
+    state.UpdateBlankPosition();
+}
+}  // namespace stp::util
+
+#endif  // RANKING_H_
